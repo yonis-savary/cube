c19f9b061f76f460df7b0d3438f6bc2b88de3fc7
yonis-savary <yonissavary@outlook.fr>
2025-01-09 23:15:01 +0100

    [WIP] env helper !

---
+++ src/Helpers/env.php
@@ -0,0 +1,16 @@
+<?php
+
+use YonisSavary\Cube\Env\Environment;
+
+if (!function_exists("env"))
+{
+    /**
+     * Return a value (by default from your `.env` file)
+     *
+     * **Please use it only inside your configuration file in case this one is cached**
+     */
+    function env(string $key, mixed $default=null): mixed
+    {
+        return Environment::getInstance()->get($key, $default);
+    }
+}
