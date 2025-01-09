3bcb1d839d12e357bbbd9fc882cbcfa4acd9dd54
yonis-savary <yonissavary@outlook.fr>
2025-01-09 23:14:55 +0100

    [WIP] Logger update

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -5,7 +5,9 @@ namespace YonisSavary\Cube\Logger;
 use Psr\Log\AbstractLogger;
 use RuntimeException;
 use Stringable;
+use Throwable;
 use YonisSavary\Cube\Core\Component;
+use YonisSavary\Cube\Data\Bunch;
 use YonisSavary\Cube\Env\Storage;
 
 class Logger extends AbstractLogger
@@ -19,24 +21,26 @@ class Logger extends AbstractLogger
 
     public static function getDefaultInstance(): static
     {
-        return new self("cube.csv", Storage::getInstance()->getSubStorage("Logs"));
+        return new self("cube.csv");
     }
 
-    public function __construct(string $file, Storage $storage)
+    public function __construct(string $file, ?Storage $storage=null)
     {
-        $this->file = $storage->path($file);
+        $storage ??= Storage::getInstance()->child("Logs");
+        $this->file = $file = $storage->path($file);
         if ($existing = self::$loggers[$file] ?? false)
             return $existing;
 
         self::$loggers[$file] = &$this;
+        $newFile = !is_file($file);
+
         if (! $stream = fopen($file, "a"))
             throw new RuntimeException("Could not open file [$file] in append mode !");
 
         $this->stream = $stream;
 
-        fputcsv($this->stream, [
-            "Datetime", "Level", "Message"
-        ]);
+        if ($newFile)
+            fputcsv($this->stream, ["Datetime", "Level", "Message"], separator: "\t", enclosure:"'", escape:"\\");
     }
 
     public function __destruct()
@@ -49,7 +53,7 @@ class Logger extends AbstractLogger
     {
         $message = (string) $message;
         foreach ($context as $key => $value)
-            $message = str_replace("{$key}", $value, $message);
+            $message = str_replace("{".$key."}", $value, $message);
 
         return $message;
     }
@@ -61,8 +65,16 @@ class Logger extends AbstractLogger
             date("Y-m-d H:i:s"),
             $level,
             $message
-        ]);
+        ], separator: "\t", enclosure:"'", escape:"\\");
     }
 
+    public function logThrowable(Throwable $thrown): void
+    {
+        $this->error("Got {type} : {message}", ["type"=>$thrown::class, "message"=>$thrown->getMessage()]);
+        $this->error("## {file}({line})", ["file" => $thrown->getFile(), "line" => $thrown->getLine()]);
+        $trace = explode("\n", $thrown->getTraceAsString());
+        foreach ($trace as $line)
+            $this->error($line);
+    }
 
 }
