c22d3ec51f4e9351b6ad8a5d3fdc40c55231d09e
yonis-savary <yonissavary@outlook.fr>
2025-02-13 23:21:54 +0100

    (WIP) Data Files

--- src/Data/Bunch.php
+++ src/Data/Bunch.php
@@ -4,10 +4,21 @@ namespace YonisSavary\Cube\Data;
 
 use YonisSavary\Cube\Utils\Utils;
 
+/**
+ * @template TKey
+ * @template TValue
+ */
 class Bunch
 {
+    /** @var array<TKey,TValue> */
     protected array $data;
 
+    /**
+     * @template TType
+     * @template TTypeKey
+     * @param array<TType>|TType|Bunch<TTypeKey,TType> $element
+     * @return self<int,TType>
+     */
     public static function of(mixed $element): self
     {
         if ($element instanceof Bunch)
@@ -19,26 +30,52 @@ class Bunch
         return new self($element);
     }
 
+    /**
+     * @template TType
+     * @return self<int,TType>
+     */
     public static function fill(int $count, mixed $value): self
     {
         return new self(array_fill(0, $count, $value));
     }
 
+    /**
+     * @return self<int,int>
+     */
     public static function range(int $end, int $start=1, int $step=1): self
     {
         return self::of(range($start, $end, $step));
     }
 
+    /**
+     * @template TType
+     * @template TTypeKey
+     * @param array<TTypeKey,TType>
+     * @return self<int,TType>
+     */
     public static function fromValues(array $assoc): self
     {
         return Bunch::of(array_values($assoc));
     }
 
+    /**
+     * @template TType
+     * @template TTypeKey
+     * @param array<TTypeKey,TType>
+     * @return self<int,TTypeKey>
+     */
     public static function fromKeys(array $assoc): self
     {
         return Bunch::of(array_keys($assoc));
     }
 
+    /**
+     * @template TArrayKey
+     * @template TArrayValue
+     *
+     * @param array<TArrayKey,TArrayValue> $assoc
+     * @return Bunch<int,array{TArrayKey,TArrayValue}>
+     */
     public static function unzip(array $assoc): self
     {
         $data = [];
@@ -52,6 +89,36 @@ class Bunch
         return self::of($data);
     }
 
+    /**
+     * @template TReturnKey
+     * @template TReturnValue
+     * @param null|\Closure(TValue):array<TReturnKey|TReturnValue> $mapFunction
+     */
+    public function zip(?callable $mapFunction=null): array
+    {
+        $clone = Bunch::of($this);
+        if ($mapFunction)
+            $clone->map($mapFunction);
+
+        return array_combine(
+            Bunch::of($clone)->map(fn($x) => $x[0])->toArray(),
+            Bunch::of($clone)->map(fn($x) => $x[1])->toArray(),
+        );
+    }
+
+    /**
+     * @return self<int,<string></string>
+     */
+    public static function fromExplode(string $delimiter, string $string): self
+    {
+        return self::of(explode($delimiter, $string));
+    }
+
+    /**
+     * @template TType
+     * @param array<TType> $initialData
+     * @return self<int,TType>
+     */
     public function __construct(array $initialData=[])
     {
         $this->data = $initialData;
@@ -62,6 +129,9 @@ class Bunch
         return new self($this->get());
     }
 
+    /**
+     * @return array<TKey,TValue>
+     */
     public function get(): array
     {
         return $this->data;
@@ -69,6 +139,7 @@ class Bunch
 
     /**
      * Alias of `get()`
+     * @return array<TKey,TValue>
      */
     public function toArray(): array
     {
@@ -81,19 +152,38 @@ class Bunch
         return $this;
     }
 
+    /**
+     * @return self<int>
+     */
     public function asIntegers(): self
     {
         return $this->filter(fn($x) => is_numeric($x))->map(fn($x) => (int) $x);
     }
 
+    /**
+     * @return self<float>
+     */
     public function asFloats(): self
     {
         return $this->filter(fn($x) => is_numeric($x))->map(fn($x) => (float) $x);
     }
 
+    /**
+     * @return static
+     */
     public function filter(?callable $callback=null): self
     {
         return $this->withNewData(array_filter($this->data, $callback));
+    }
+
+    /**
+     * @template X
+     * @param string|X $class
+     * @return self<X>
+     */
+    public function onlyInstancesOf(string $class): self
+    {
+        return $this->filter(fn($x) => $x instanceof $class);
     }
 
     public function partitionFilter(callable $callback): array
@@ -109,11 +199,31 @@ class Bunch
         return $output;
     }
 
+    /**
+     * @template TReturnValue
+     * @param \Closure(TValue):TReturnValue $callback
+     * @return Bunch<TKey,TReturnValue>
+     */
     public function map(callable $callback): self
     {
         return $this->withNewData(array_map($callback, $this->data));
     }
 
+    public function flat(): self
+    {
+        $data = [];
+        foreach ($this->data as $array)
+            array_push($data, ...$array);
+
+        return $this->withNewData($data);
+    }
+
+    /**
+     * @template TMergedKey
+     * @template TMergedValue
+     * @param array<TMergedKey,TMergedValue>|Bunch<TMergedKey,TMergedValue> $value
+     * @return Bunch<TKey|TMergedKey,TValue|TMergedValue>
+     */
     public function merge(array|Bunch $value): self
     {
         return $this->withNewData(
@@ -121,6 +231,9 @@ class Bunch
         );
     }
 
+    /**
+     * @param \Closure(TValue) $callback
+     */
     public function sort(callable|int $callbackOrSortMode=SORT_REGULAR): self
     {
         is_callable($callbackOrSortMode) ?
@@ -129,11 +242,18 @@ class Bunch
         return $this->withNewData($this->data);
     }
 
-    public function forEach(callable $callback): void
+    /**
+     * @param \Closure(TValue) $callback
+     */
+    public function forEach(callable $callback): self
     {
         array_walk($this->data, $callback);
+        return $this;
     }
 
+    /**
+     * @param \Closure(TValue) $callback
+     */
     public function any(callable $callback): bool
     {
         foreach ($this->data as $element)
@@ -144,6 +264,9 @@ class Bunch
         return false;
     }
 
+    /**
+     * @param \Closure(TValue) $callback
+     */
     public function all(callable $callback): bool
     {
         foreach ($this->data as $element)
@@ -190,6 +313,9 @@ class Bunch
         return join($glue, $this->data);
     }
 
+    /**
+     * @param \Closure(TValue) $callback
+     */
     public function first(callable $callback): mixed
     {
         foreach ($this->data as $element)
@@ -198,6 +324,21 @@ class Bunch
                 return $element;
         }
         return null;
+    }
+
+    /**
+     * @param \Closure(TValue):bool $callback
+     */
+    public function firstIndex(callable $callback): int
+    {
+        $dataCount = count($this->data);
+        for ($i=0; $i<$dataCount; $i++)
+        {
+            $element = $this->data[$i];
+            if ($callback($element) === true)
+                return $i;
+        }
+        return -1;
     }
 
     public function has(mixed $value): bool
@@ -216,6 +357,11 @@ class Bunch
         return $this;
     }
 
+    /**
+     * @template TAcc
+     * @param \Closure(TAcc,TValue):TAcc $callback
+     * @param TAcc $start
+     */
     public function reduce(callable $callback, mixed $start=0): mixed
     {
         $acc = $start;
