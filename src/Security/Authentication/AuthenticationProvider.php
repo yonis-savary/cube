916c3ec4c118df4af5c6c61d17f8a3820767b95f
yonis-savary <yonissavary@outlook.fr>
2025-08-25 23:12:44 +0200

    AuthenticationProvider interface !

---
+++ src/Security/Authentication/AuthenticationProvider.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace Cube\Security\Authentication;
+
+use Cube\Data\Models\Model;
+
+interface AuthenticationProvider {
+    public function attempt(string $identifier, ?string $password=null): Model|false;
+}
---
+++ src/Security/Authentication/PasswordAuthentication.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace Cube\Security\Authentication;
+
+use Cube\Core\Autoloader;
+use Cube\Data\Database\Database;
+use Cube\Data\Models\Model;
+use Cube\Security\Authentication\Events\FailedAuthentication;
+use Cube\Utils\Utils;
+
+class PasswordAuthentication implements AuthenticationProvider
+{
+    /**
+     * @param class-string<Model> $model
+     */
+    public function __construct(
+        public readonly string $model,
+        public readonly array|string $loginFields,
+        public readonly string $passwordField,
+        public readonly ?string $saltField = null,
+        protected ?Database $database = null
+    ) {
+        if (!Autoloader::extends($model, Model::class)) {
+            throw new \InvalidArgumentException("{$model} class does not extends Model");
+        }
+
+        $this->database ??= Database::getInstance();
+    }
+
+    public function attempt(string $identifier, ?string $password = null): Model|false
+    {
+        $model = $this->model;
+
+        $loginFields = Utils::toArray($this->loginFields);
+        $query = $model::select();
+
+        foreach ($loginFields as $field) {
+            $query->where($field, $identifier, '=', $model::table())->or();
+        }
+
+        if (!$user = $query->first()) {
+            return false;
+        }
+
+        $passwordField = $this->passwordField;
+        $hash = $user->{$passwordField};
+
+        $this->saltString($password, $user);
+
+        if (!password_verify($password, $hash)) {
+            (new FailedAuthentication())->dispatch();
+
+            return false;
+        }
+
+        return $user;
+    }
+
+    public function saltString(string &$string, Model $user): void
+    {
+        if ($saltField = $this->saltField) {
+            $string .= $user->{$saltField};
+        }
+    }
+}
