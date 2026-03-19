ba314ff12e838078e779e7ab07c4e906f07b0aa6
yonis-savary <yonissavary@outlook.fr>
2026-03-19 15:02:10 +0100

    better Queue documentation

--- docs/205-schedule-and-queues.md
+++ docs/205-schedule-and-queues.md
@@ -4,5 +4,89 @@
 
 (WIP)
 
+## Queues 
+
+Queues are a great way to process data asynchronously
+
+To create a queue, create a Class extending the abstract `Queue` class
+
+```php
+class CalculatorQueue extends Queue {
+    public function __invoke(int $a, int $b) {
+        $this->logger->info($a + $b);
+    }
+}
+```
+
+Then, you can push data either by using `push` or `queue`
+
+```php
+CalculatorQueue::queue(2,3);
+// OR
+$queue = new CalculatorQueue();
+$queue->push(2, 3);
+```
+
+Finally, you can launch your queue with the `queue` command
+
+```bash
+php do cube:queue --queue=CalculatorQueue
+
+# You can use the -l flag to attach the global logger to stdOut
+# Useful when launched as docker service
+php do cube:queue --queue=CalculatorQueue -l
+```
+
+In our example, you will notice that a `calculatorqueue.csv` got created in `Storage/Logs`, this file is used
+as global log file as long as your queue runs
+
+## Customizing Queue class 
+
+You can customize your queue behavior by overriding these methods
+
+```php
+class CalculatorQueue extends Queue {
+    /**
+     * You can edit the way the Queue store jobs
+     * Using Redis is advised for medium|large-sized applications
+     * (by default the local disk is used)
+     * You can find a basic Redis docker service at the end of this document
+     */
+    protected function getDriver(): QueueDriver {
+        return new RedisQueue();
+    }
+
+    /**
+     * This method shall be called when a exception is raised
+     * when processing a queue item
+     *
+     * @return bool If `true`, the system will re-push the failed job to the queue, otherwise, the job is cancelled
+     */
+    protected function onError(Throwable $thrown, array $args): bool
+    {
+        // In this exemple, we log the exception and delete the failed job
+        $this->logger->logThrowable($thrown);
+        return false;
+    }
+
+    public function __invoke(int $a, int $b) {
+        $this->logger->info($a + $b);
+    }
+}
+```
+
+## Clearing/Flushing Queue 
+
+To clear your Queue, you can either call the `queue` command with the `-f` flag, or call the `flush` method 
+
+```bash
+php do cube:queue --queue=CalculatorQueue -f
+```
+
+or
+
+```php
+$calculatorqueue->flush();
+```
 
 <!-- menu --><table style='width:100%'><tr><td style='width: 33%'><div style="text-align: left"><a href="./204-http-client.md">Previous : Http client</a></div></td><td style='width: 33%; text-align: center'><div style="Center"><a href="./README.md"> Readme</a></div></td><td style='width: 33%'><div style="text-align: right"><a href="./README.md">Next : Readme</a></div></td></tr></table>
--- src/Queue/Queue.php
+++ src/Queue/Queue.php
@@ -13,24 +13,23 @@ use RuntimeException;
 use Throwable;
 
 /**
- * To implement your queue, you should implement
+ * Abstract Class to implement Queueing system
  *
- * `public function __invoke($customArgs, $customArgs...)` to process singular items
+ * How to implements:
+ * - Item processor : `public function __invoke($customArgs, $customArgs...)`
+ * - Set storage type : `protected function getDriver(): QueueDriver` (local disk by default)
  *
- * `protected function getDriver(): QueueDriver` to specify how the queue store items (localdisk driver by default)
+ * Basic interactions
+ * - Push items (static) : `YourQueueClass::queue($customArgs, $customArgs)`
+ * - Push items : `$yourQueue->push($customArgs, $customArgs)`
+ * - Launch `php do cube:queue --queue=App\Queues\YourQueueClass`
  *
- * `protected function onError(Throwable $thrown, array $args): bool` to specify how the queue handle errors
- *
- * To interact with your queue, you can call
- *
- * Launch `php do cube:queue --queue=App\Queues\YourQueueClass` to launch it
- * Launch `php do cube:queue --queue=App\Queues\YourQueueClass --flush` to flush it
- *
- * `YourQueueClass::queue($customArgs, $customArgs)` to push items
- * `$yourQueue->push($customArgs, $customArgs)` to push items (alternative to static call)
- * `$yourQueue->flush()` to clear the queue
- * `$yourQueue->processNext()` if you need to process exactly one element
- * `loop(?Logger $attachedLogger=null)` if you wish to manually launch the queue
+ * Advanced interactions
+ * - Define error behavior: `protected function onError(Throwable $thrown, array $args): bool`
+ * - Flush `php do cube:queue --queue=App\Queues\YourQueueClass --flush`
+ * - Clear the queue : `$yourQueue->flush()`
+ * - Process one element : `$yourQueue->processNext()`
+ * - Manually launch the queue : `loop(?Logger $attachedLogger=null)`
  */
 abstract class Queue
 {
@@ -58,14 +57,15 @@ abstract class Queue
         $this->logger = $this->getLogger() ?? new NullLogger();
     }
 
-    protected function assertIsCallable() {
+    protected function assertIsCallable()
+    {
         if (!method_exists($this, '__invoke'))
             throw new RuntimeException("__invoke method must be instanciated on class");
     }
 
     protected function getDriver(): QueueDriver
     {
-        return new LocalDiskQueueDriver(static::class);
+        return new LocalDiskQueueDriver();
     }
 
     /**
@@ -74,11 +74,13 @@ abstract class Queue
      *
      * @return bool On `true`, the system will repush the failed job on queue, otherwise, the job is cancelled
      */
-    protected function onError(Throwable $thrown, array $args): bool {
+    protected function onError(Throwable $thrown, array $args): bool
+    {
         return false;
     }
 
-    public function flush() {
+    public function flush()
+    {
         $this->initialize();
         return $this->driver->flush();
     }
@@ -86,7 +88,8 @@ abstract class Queue
     /**
      * @return mixed $args Arguments that shall be passed to `__invoke` when processing the item
      */
-    public function push(mixed ...$args) {
+    public function push(mixed ...$args)
+    {
         $this->initialize();
         return $this->driver->push($args);
     }
